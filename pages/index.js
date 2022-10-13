337845b82bac055ee84b5c51468bee0a3cd3d4a8
KevinSequeiraG <kevinsteven.07.sg@gmail.com>
2022-10-12 21:28:48 -0500

    cambios

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { NavBar } from '../components/navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="bg-black">
-      <h1 className='text-[58rem] text-red-500'>asfdasfas</h1>
+    <div className="bg-black bgMain w-screem h-screen">
+      <NavBar/>
     </div>
   )
 }
