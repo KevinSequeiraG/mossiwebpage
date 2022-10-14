01a178e68ffbaac53bc3ee47173bef54d4593fe3
KevinSequeiraG <kevinsteven.07.sg@gmail.com>
2022-10-13 23:55:00 -0500

    new components

---
+++ components/newCategoryModal.js
@@ -0,0 +1,11 @@
+const NewCategoryModal = () => {
+    return (
+        <div className="w-[40%] h-[40%] bg-green-800 absolute top-48 z-[1000] left-[30%] rounded-lg">
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default NewCategoryModal;
