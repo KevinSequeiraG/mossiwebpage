c551824d02b42befcbeeeb520dc55adbb55e1f39
KevinSequeiraG <kevinsteven.07.sg@gmail.com>
2022-10-14 01:36:12 -0500

    asdf

--- components/newCategoryModal.js
+++ components/newCategoryModal.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useForm } from 'react-hook-form';
+import {
+    collection,
+    addDoc
+} from "firebase/firestore";
+import { database } from "../lib/firebaseConfig";
+import Swal from "sweetalert2";
 
 const NewCategoryModal = (props) => {
     const { register, handleSubmit, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
-    console.log(errors);
+    const [categoryName, setCategoryName] = useState("");
+    const [categoryDescription, setCategoryDescription] = useState("");
+    const [categoryImage, setCategoryImage] = useState();
+
+    const onSubmit = data => {createCategory(data)}
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 2000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener("mouseenter", Swal.stopTimer);
+            toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+    });
+
+    const createCategory = (data) => {
+        const categoryRef = collection(database, `mossy/data/category`);
+        addDoc(categoryRef, {
+            name: data.categoryName,
+            description: data.categoryDescription,
+            image: `${data.categoryImage != undefined ? data.categoryImage : ''}`
+        })
+            .then(() => {
+                Toast.fire({
+                    icon: "success",
+                    title: "Categoria creada con éxito", //`${t("toastCreateSuccess")}`
+                });
+            }).then(
+                props.closeModal(),
+                props.getCategoryData()
+            )
+            .catch((err) => {
+                console.error(err);
+                Toast.fire({
+                    icon: "error",
+                    title: `${t("toastCreateFail")}`,
+                });
+            });
+    };
 
     return (
         <div className="w-[40%] h-auto bg-gray-800 border-2 border-gray-300 absolute top-1/2 left-1/2 z-[1000] translate-x-[-50%] translate-y-[-50%] rounded-lg py-5">
-            <button onClick={()=>{props.closeModal()}} className="absolute -right-10 -top-10 bg-red-500 text-white rounded-[50%] border border-white w-[4rem] z-[9999] h-[4rem] mx-2 hover:bg-red-800">
+            <button onClick={() => { props.closeModal() }} className="absolute -right-10 -top-10 bg-red-500 text-white rounded-[50%] border border-white w-[4rem] z-[9999] h-[4rem] mx-2 hover:bg-red-800">
                 <span className="material-icons !text-[50px]">
                     close
                 </span>
@@ -18,17 +64,13 @@ const NewCategoryModal = (props) => {
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-4/5 mx-auto mt-8">
                     <div className='flex justify-between my-2'>
                         <label className='text-white'>Nombre de categoría</label>
-                        <input className='rounded' type="text" placeholder="Nombre" {...register("Nombre", { required: true, maxLength: 80 })} />
+                        <input className='rounded' type="text" placeholder="Nombre" {...register("categoryName", { required: true, maxLength: 80 })} />
                     </div>
                     <div className='flex justify-between my-2'>
                         <label className='text-white'>Descripción de categoría</label>
-                        <input className='rounded' type="text" placeholder="Descripción" {...register("Descripción", { required: true, maxLength: 100 })} />
+                        <input className='rounded' type="textarea" placeholder="Descripción" {...register("categoryDescription", { required: true, maxLength: 100 })} />
                     </div>
-                    <div className='flex justify-between my-2'>
-                        <label className='text-white'>Descripción de categoría</label>
-                        
-                    </div>
-                    <input className="text-[1rem] mt-2 lg:mt-8 px-4 py-2 bg-green-500 text-white rounded-xl hover:bg-green-700 mr-2" type="submit" value={"Guardar categoría"}/>
+                    <input className="text-[1rem] mt-2 lg:mt-8 px-4 py-2 bg-green-500 text-white rounded-xl hover:bg-green-700 mr-2" type="submit" value={"Guardar categoría"} />
                 </form>
             </div>
         </div>
