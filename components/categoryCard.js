63b750033d0d7f764447f2193c804bd5a2c04de8
OscarGSC <85893827+OscarGSC@users.noreply.github.com>
2022-10-14 00:37:31 -0600

    Sokar no lo borre

--- components/categoryCard.js
+++ components/categoryCard.js
@@ -1,10 +1,10 @@
-const CategoryCard = () => {
+const CategoryCard = (props) => {
     return (
         <div className="card flex flex-col">
             <div className="z-[2] bg-[#191c29] cardSize">
-                <img src="./Images/Panaderia.png" className="rounded-lg"></img>
-                <h1 className="text-gray-200 mt-2 px-4">Panaderia</h1>
-                <p className="text-gray-200 text-[1rem] mt-4 px-4">Encuentra en esta seccion nuestros deliciosos </p>
+                <img src={props.data.categoryImgUrl!=""?props.data.categoryImgUrl:"./Images/Panaderia.png"} className="rounded-lg"></img>
+                <h1 className="text-gray-200 mt-2 px-4">{props.data.name}</h1>
+                <p className="text-gray-200 text-[1rem] mt-4 px-4">{props.data.description}</p>
                 <button className="text-[.7rem] lg:text-[1rem] mt-8 px-4 py-2 bg-green-500 text-white rounded-xl hover:bg-green-700">Ver productos</button>
                 <div className="mt-6" >
                     <button className="bg-red-500 text-white rounded-[50%] border border-white w-[2rem] h-[2rem] mx-2 hover:bg-red-800">
