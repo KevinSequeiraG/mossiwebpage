d005d2e57f919d322eb4f6de90c8090db37114de
Erickv25 <erickvrp25@gmail.com>
2022-10-14 01:51:15 -0600

    auida

--- components/categoryCard.js
+++ components/categoryCard.js
@@ -1,30 +1,48 @@
+import Link from "next/link";
 import { useState } from "react";
 
 const CategoryCard = (props) => {
-    
-
-    return (
-        <div className="card flex flex-col">
-            <div className="z-[2] bg-[#191c29] cardSize">
-                <img src={props.data.categoryImgUrl != "" ? props.data.categoryImgUrl : "./Images/Panaderia.png"} className="rounded-lg"></img>
-                <h1 className="text-gray-200 mt-2 px-4">{props.data.name}</h1>
-                <p className="text-gray-200 text-[1rem] mt-4 px-4">{props.data.description}</p>
-                <button className="text-[.7rem] lg:text-[1rem] mt-8 px-4 py-2 bg-green-500 text-white rounded-xl hover:bg-green-700">Ver productos</button>
-                <div className="mt-6" >
-                    {props.showMaintenance ? <button className="bg-red-500 text-white rounded-[50%] border border-white w-[2rem] h-[2rem] mx-2 hover:bg-red-800">
-                        <span className="material-icons">
-                            close
-                        </span>
-                    </button> : null}
-                    {props.showMaintenance?<button className="bg-yellow-500 text-white rounded-[50%] border border-white w-[2rem] h-[2rem] mx-2 hover:bg-yellow-600">
-                        <span className="material-icons">
-                            edit
-                        </span>
-                    </button>:null}
-                </div>
-            </div>
+  return (
+    <div className="card flex flex-col">
+      <div className="z-[2] bg-[#191c29] cardSize">
+        <img
+          src={
+            props.data.categoryImgUrl != ""
+              ? props.data.categoryImgUrl
+              : "./Images/Panaderia.png"
+          }
+          className="rounded-lg"
+        ></img>
+        <h1 className="text-gray-200 mt-2 px-4">{props.data.name}</h1>
+        <p className="text-gray-200 text-[1rem] mt-4 px-4">
+          {props.data.description}
+        </p>
+        <Link
+          href={{
+            pathname: "/productList",
+            query: props.data,
+          }}
+          as={"/productList"}
+        >
+          <button className="text-[.7rem] lg:text-[1rem] mt-8 px-4 py-2 bg-green-500 text-white rounded-xl hover:bg-green-700">
+            Ver productos
+          </button>
+        </Link>
+        <div className="mt-6">
+          {props.showMaintenance ? (
+            <button className="bg-red-500 text-white rounded-[50%] border border-white w-[2rem] h-[2rem] mx-2 hover:bg-red-800">
+              <span className="material-icons">close</span>
+            </button>
+          ) : null}
+          {props.showMaintenance ? (
+            <button className="bg-yellow-500 text-white rounded-[50%] border border-white w-[2rem] h-[2rem] mx-2 hover:bg-yellow-600">
+              <span className="material-icons">edit</span>
+            </button>
+          ) : null}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default CategoryCard;
--- pages/productList/index.js
+++ pages/productList/index.js
@@ -1,10 +1,20 @@
-import React from "react";
-import CategoryCard from "../../components/categoryCard";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/footer";
 import { NavBar } from "../../components/navbar";
 import ProductCard from "../../components/productCard";
 
 export default function ProductList() {
+  const router = useRouter();
+  const [categoryData, setCategoryData] = useState(router.query);
+  useEffect(() => {
+    console.log(categoryData)
+    if (Object.entries(categoryData).length != 0) {
+      localStorage.setItem("categoryDataMemory", JSON.stringify(categoryData));
+    } else {
+      setCategoryData(JSON.parse(localStorage.getItem("categoryDataMemory")));
+    }
+  }, []);
   return (
     <>
       <NavBar />
@@ -12,16 +22,11 @@ export default function ProductList() {
       <div className={`cardsContainer w-full h-full py-20 bgMain`}>
         <div className="text-left w-10/12 mx-auto mt-8 lg:mt-16">
           <h1 className="text-[22px] lg:text-[30px] font-bold text-white text-center">
-            NOMBRE DE LA CATEGORÍA
+            {categoryData.categoryName}
           </h1>
           <hr />
           <p className="text-white font-medium text-[15px] lg:text-[18px] text-justify mb-9 lg:mb-16 mt-4 leading-6 lg:leading-7">
-            DESCRPCIÓN DE LA CATEGORÍA Nunc tellus nibh, interdum ac rhoncus et,
-            mattis egestas mauris. Sed et venenatis nisl. Morbi augue velit,
-            viverra non dignissim id, commodo nec risus. Nunc tellus nibh,
-            interdum ac rhoncus et, mattis egestas mauris. Sed et venenatis
-            nisl. Morbi augue velit, viverra non dignissim id, commodo nec
-            risus.
+            {categoryData.categoryDescription}
           </p>
         </div>
         <div className="w-10/12 h-11/12 top-40 inset-x-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1 mx-auto justify-items-center gap-y-12">
